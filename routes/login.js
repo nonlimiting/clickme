7edf8601e4eeea1dcf7e6ec99ab7e00367a7e521
helbing <414606527@qq.com>
2015-03-10 16:22:59 +0800

    check account and nickname were exist done

--- routes/login.js
+++ routes/login.js
@@ -1,9 +1,11 @@
 var express = require('express');
-var router = express.Router();
-
+var crypto = require('crypto');
+var async = require('async');
 var User = require('../models/user');
 
-var crypto = require('crypto');
+var router = express.Router();
+
+
 
 /* GET login page */
 router.get('/', function(req, res, next) { 
@@ -15,16 +17,16 @@ router.post('/', function(req, res, next) {
 	var md5 = crypto.createHash('md5');
 	md5.update(req.body.pwd);
 	var md5pwd = md5.digest('hex');
-	User.find({ account: req.body.account, pwd: md5pwd}, function(err, data) { 
+	User.findOne({ account: req.body.account, pwd: md5pwd}, function(err, data) { 
 		if(err) { 
 			return res.render('error', { message: '系统出错，请稍后登陆' });
 		}
 		else { 
-			if(data != '') { 
+			if(data != null) {
 				return res.redirect('/');
 			}
 			else { 
-				return res.render('error', { message: '账号或密码错误' });
+				return res.render('error', { message: '账号或密码错误' });	
 			}
 		}
 	});
@@ -37,36 +39,64 @@ router.get('/reg', function(req, res, next) {
 
 /* POST register handle */
 router.post('/reg', function(req, res, next) { 
-	var md5 = crypto.createHash('md5');
-	md5.update(req.body.pwd);
-	var md5pwd = md5.digest('hex');
-	var user = new User({ 
-		account: req.body.account,
-		pwd: md5pwd,
-		info: { 
-			nickname: req.body.nickname,
-			sex: req.body.sex,
-			intro: req.body.intro
-		}
-	});
-	user.save(function(err, data) { 
-		if(err) { 
-			return res.render('error', { message: '注册失败，请重试' });
-		}
-		else { 
-			return res.redirect('/login');
-		}
-	});
-});
 
-/* check account is exist */
-router.post('/chkAccount', function(req, res, next) { 
-	res.send('check account is exist');
-});
-
-/* check nickname is exist */
-router.post('/chkNickname', function(req, res, next) { 
-	res.send('check nickname is exist');
+	async.waterfall([
+		function(cb) { 
+			// check account is exist
+			User.findOne({ account: req.body.account }, function(err, data) { 
+				if(err) {
+					return res.render('error', { message: '注册失败，请重试' });
+				}
+				else { 
+					if(data != null) {
+						return res.render('error', { message: '账号已存在'});
+					}
+					else { 
+						cb(null);
+					}
+				}
+			});
+		},
+		function(cb) { 
+			// check nickname is exist
+			User.findOne({ 'info.nickname': req.body.nickname }, function(err, data) { 
+				console.log(data);
+				if(err) {
+					return res.render('error', { message: '注册失败，请重试' });
+				}
+				else {
+					if(data != null) {
+						return res.render('error', { message: '昵称已存在'});
+					}
+					else { 
+						cb(null);
+					}
+				}
+			});
+		}
+	], function() { 
+		// register doing
+		var md5 = crypto.createHash('md5');
+		md5.update(req.body.pwd);
+		var md5pwd = md5.digest('hex');
+		var user = new User({ 
+			account: req.body.account,
+			pwd: md5pwd,
+			info: { 
+				nickname: req.body.nickname,
+				sex: req.body.sex,
+				intro: req.body.intro
+			}
+		});
+		user.save(function(err, data) { 
+			if(err) { 
+				return res.render('error', { message: '注册失败，请重试' });
+			}
+			else { 
+				return res.redirect('/login');
+			}
+		});
+	});
 });
 
 module.exports = router;
